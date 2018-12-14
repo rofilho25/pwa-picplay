7dc439cc7bba7c57c41eae4c636c8014a3e37a4b
Beto <rofilho@jc.com.br>
2018-12-14 17:22:55 -0200

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+html{
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+body{
+  background: #999bc691;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: linear-gradient(to bottom, rgba(236, 209, 214, 0.95) 0%, rgba(74, 7, 1, 0.09) 100%);
+  height: 100%;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Ranking.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="rank">
+    <div class="head">
+
+    </div>
+    <div class="body">
+      <div class="scroll">
+      <div class="panel">
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      <div class="player"></div>
+      </div>
+    </div>
+      </div>
+    <div class="footer">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ $widthRank : 60px;
+ $backgroundColor:transparent;
+
+.rank{
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  .head{
+    width: $widthRank;
+    background-color: $backgroundColor;
+    border-radius: 50%;
+    height: $widthRank;
+    position: absolute;
+    top: -35px;
+    border: aliceblue;
+    border-style: solid;
+    border-top-left-radius: 191px;
+    border-top-right-radius: 191px;
+    border-bottom: 0;
+
+  }
+  .body{
+    width: $widthRank;
+    background-color: $backgroundColor;
+    height: 300px;
+    border: aliceblue;
+    border-style: solid;
+    z-index: 1000;
+    border-top: transparent;
+    border-bottom: transparent;
+    position: relative;
+
+  }
+  .panel{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    :first-child{
+    height: 49px;
+    width: 49px;
+    border-radius: 50%;
+  }
+  }
+  .footer{
+    width: $widthRank;
+    height: $widthRank;
+    border-radius: 50%;
+    background-color: $backgroundColor;
+    position: absolute;
+    bottom: -35px;
+    border: aliceblue;
+    border-style: solid;
+    border-bottom-left-radius: 191px;
+    border-bottom-right-radius: 191px;
+    border-top: 0;
+
+  }.player{
+    height: 35px;
+    width: 35px;
+    border-radius: 50%;
+    background-color: white;
+    margin-bottom: 5px;
+  
+  }
+  .scroll{
+  overflow-y: auto;
+    height: 359px;
+    position: absolute;
+    top: -34px;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 31px;
+    padding-top: 9px;
+    -webkit-border-radius: 31px;
+    z-index: 111111;
+    border-radius: 31px;
+
+  /* Firefox 1-3.6 */
+  -moz-border-radius: 31px; 
+  
+  /* Opera 10.5, IE 9, Safari 5, Chrome, Firefox 4, iOS 4, Android 2.1+ */
+  border-radius: 31px; 
+  }
+::-webkit-scrollbar { 
+    display: none; 
+}
+}
+</style>
